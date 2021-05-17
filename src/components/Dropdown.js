3906f72bb2af4bd8b39097a5a60610a7af15de91
Eugene Yehanovskiy <yehanovskiy@gmail.com>
2021-05-17 14:24:43 +0300

    translate widget done

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
 
@@ -33,7 +33,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a color</label>
+                <label className="label">{label}</label>
                 <div onClick={() => setOpen(!open)} 
                     className={`ui selection dropdown ${open ? 'visible active' : ''}`}
                 >
@@ -44,7 +44,6 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
                     </div>
                 </div>
             </div>
-            <h3 style={{color: selected.value}}>The text is {selected.value}</h3>
         </div>
     );
 };
